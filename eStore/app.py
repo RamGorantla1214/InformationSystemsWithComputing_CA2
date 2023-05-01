7a6969ee2c8351af451f1e249abc480033ae072c
RamGorantla1214 <78721428+RamGorantla1214@users.noreply.github.com>
2023-05-02 00:53:49 +0100

    added home page
    1. configured reusable header template
    2. created css file

--- eStore/app.py
+++ eStore/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
 @app.route('/')
 def hello():
- return 'Hello world'
+ return render_template('home.html')
 if __name__ == '__main__':
  app.run(host='127.0.0.1', port=8080, debug=True)
